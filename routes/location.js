dde34ce03243abc02d0574b2bd73ef54aaa66e2c
anvcodes <kpeace0720@gmail.com>
2019-11-08 12:41:58 -0500

    travis issues

--- routes/location.js
+++ routes/location.js
@@ -1,23 +1,21 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
+// router.get("/routes/location", (req, res) => {
+//     console.log("hello world");
+//     // res.send(`a get request with /user route on port ${PORT}`);
+// })
 
-router.get("/routes/location", (req, res) => {
-    console.log("hello world");
-    // res.send(`a get request with /user route on port ${PORT}`);
-})
+// router.post("/api/location/post", (req, res) => {
+//     res.send(`a post request with /user/post route on port ${PORT}`);
+// })
 
-router.post("/api/location/post", (req, res) => {
-    res.send(`a post request with /user/post route on port ${PORT}`);
-})
+// router.put("/api/location/put", (req, res) => {
+//     res.send(`a put request with /user/put route on port ${PORT}`);
+// })
 
-router.put("/api/location/put", (req, res) => {
-    res.send(`a put request with /user/put route on port ${PORT}`);
-})
+// router.delete("/api/location/delete", (req, res) => {
+//     res.send(`a delete request with /user/delete route on port ${PORT}`);
+// })
 
-router.delete("/api/location/delete", (req, res) => {
-    res.send(`a delete request with /user/delete route on port ${PORT}`);
-})
-
-
-module.exports = router;
+// module.exports = router;
