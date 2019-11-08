b94db884c67f51559696c63798628e94ff34a567
anvcodes <kpeace0720@gmail.com>
2019-11-08 11:21:44 -0500

    test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,22 +5,22 @@
 //   app.get("/", function(req, res) {
 //     // db.User.findAll({}).then(function(answer) {
 //     //   res.render("profileBuilder.handlebars", {
-      
+
 //     //   });
 //     // });
 
 //   });
 
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
+// Load example page and pass in an example by id
+// app.get("/example/:id", function(req, res) {
+//   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//     res.render("example", {
+//       example: dbExample
+//     });
+//   });
+// });
 
-  // Render 404 page for any unmatched routes
+// Render 404 page for any unmatched routes
 //   app.get("*", function(req, res) {
 //     res.render("404");
 //   });
