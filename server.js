608cb78d8ee45a898f276cf11764ad3b1f4cee1c
Logan <wislermichel@gmail.com>
2019-11-08 00:04:01 -0500

    updated server page

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 
 
-// user routes
+// routes
 var user = require("./routes/user.js");
 var location = require("./routes/location.js");
 app.use("/user", user);
