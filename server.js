2f11bf2e5c645483ddbf3abb9e118a4f0ac0ca36
Jon Perkins <joso1958@gmail.com>
2019-11-11 11:48:51 -0500

    updated layout of profile page and fixed some alignment issues form messages and matches pages on mobile. added some styling to headings on each page

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const express = require("express");
-const path = require("path");
-const hbs = require("express-handlebars");
-
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // require("dotenv").config();
 // var keys = require("./keys.js");
+app.use(express.static('public'));
 
-app.engine("handlebars", hbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
+app.engine('handlebars', hbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // routes
-var user = require("./routes/user.js");
-var location = require("./routes/location.js");
-var htmlRoute = require("./routes/html/htmlRoute.js");
+var user = require('./routes/user.js');
+var location = require('./routes/location.js');
+var htmlRoute = require('./routes/html/htmlRoute.js');
 // app.use("/user", user);
 // app.use("/location", location);
-app.use("/", htmlRoute);
-
+app.use('/', htmlRoute);
 
 app.listen(PORT, function() {
-    console.log("App is listening on port " + PORT);
+	console.log('App is listening on port ' + PORT);
 });
-
